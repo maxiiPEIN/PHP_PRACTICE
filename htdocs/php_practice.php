de2996af98b2bb6eed39828d89d681809bee9b37
maxiiPEIN <162523584+maxiiPEIN@users.noreply.github.com>
2025-08-27 19:39:05 +0800

    Add files via upload

--- htdocs/php_practice.php
+++ htdocs/php_practice.php
@@ -66,33 +66,158 @@
     // $a = 5;
     // echo "$a Eggs";
 
-    $a = "John";
-    $b = -5;
-    $c = 3.14;
-    $d = false;
-    $e = [1,2,3];
-    $f = new stdClass();
-    $g = null;
-    $h = fopen("php_practice.php", "r");
+    // $a = "John";
+    // $b = -5;
+    // $c = 3.14;
+    // $d = false;
+    // $e = [1,2,3];
+    // $f = new stdClass();
+    // $g = null;
+    // $h = fopen("php_practice.php", "r");
 
-    var_dump($a);
-    echo 'br';
-    var_dump($b);
-    echo 'br';
-    var_dump($c);
-    echo 'br';
-    var_dump($d);
-    echo 'br';
-    var_dump($e);
-    echo 'br';
-    var_dump($f);
-    echo 'br';
-    var_dump($g);
-    echo 'br';
-    var_dump($h);
-    echo 'br';
+    // var_dump($a);
+    // echo '<br>';
+    // var_dump($b);
+    // echo '<br>';
+    // var_dump($c);
+    // echo'<br>';
+    // var_dump($d);
+    // echo '<br>';
+    // var_dump($e);
+    // echo'<br>';
+    // var_dump($f);
+    // echo '<br>';
+    // var_dump($g);
+    // echo '<br>';
+    // var_dump($h);
+    // echo'<br>';
 
     
+
+    // echo strlen($a), "<br>";
+    // echo str_word_count($a), "<br>";
+    // echo strpos($a, "Cruz"), "<br>";
+    // echo strtoupper($a), "<br>";
+    // echo strtolower($a), "<br>";
+    // echo str_replace("Cruz", "Manaloto", $a), "<br>";
+    // echo strrev($a), "<br>";
+    // echo trim("Hello Love!"), "<br>";
+    // var_dump (explode("o","Hello Love!"));
+    
+    //echo var_dump("Hello" . "World!");
+
+    // $x = "Hello";
+    // $y = "World";
+
+    // echo $x ." ". $y, "<br>";
+    // echo "$x $y", "<br>";
+    // echo "John \" Bay\" Cruz", "<br>";
+    // echo 'John \' Bay\'Cruz', "<br>";
+    // echo "John \$ Bay\" Cruz", "<br>";
+
+    // $a = 5;
+    // $b = 3.14;
+    // $c = "25";
+
+
+    // echo is_int($a), "<br>";
+    // echo is_int($b), "<br>";
+    // echo is_int($c), "<br>";
+
+
+    // echo PHP_FLOAT_MAX, "<br>";
+    // echo PHP_FLOAT_MIN, "<br>";
+    // echo PHP_FLOAT_DIG, "<br>"; 
+    // echo PHP_FLOAT_EPSILON, "<br>";
+
+
+    // echo is_float($a), "<br>";
+    // echo is_float($b), "<br>";
+    // echo is_float($c), "<br>";
+
+
+    // echo is_numeric($a), "<br>";
+    // echo is_numeric($b), "<br>";
+    // echo is_numeric($c), "<br>";
+
+
+    // $a = (string) "John";
+    // $b = (string) -5;
+    // $c = (string) 3.14;
+    // $d = (string) false;
+    // $e = (string) null;
+    // $f = (string) fopen("php_practice.php", "r");
+
+
+    // $a = (int) "John Cruz";
+    // $b = (int) -5;
+    // $c = (int) 3.14;
+    // $d = (int) true;
+    // $e = (int) null;
+    // $f = (int) fopen("php_practice.php", "r");
+
+
+    // $a = (float) "5John Cruz";
+    // $b = (float) -5;
+    // $c = (float) 3.14;
+    // $d = (float) false;
+    // $e = (float) null;
+    // $f = (float) fopen("php_practice.php", "r");
+
+    // $a = (bool) "John Cruz";
+    // $b = (bool) -5;
+    // $c = (bool) 3.14;
+    // $d = (bool) true;
+    // $e = (bool) null;
+    // $f = (bool) fopen("php_practice.php", "r");
+
+
+    //  $a = (array) "John Cruz";
+    // $b = (array) -5;
+    // $c = (array) 3.14;
+    // $d = (array) false;
+    // $e = (array) null;
+    // $f = (array) fopen("php_practice.php", "r");
+
+
+    // $a =  "John Cruz";
+    // $b =  -5;
+    // $c =  3.14;
+    // $d =  true;
+    // $e =  null;
+    // $f =  fopen("php_practice.php", "r");
+
+    // unset($a);
+
+    // var_dump($a);
+    // echo '<br>';
+    // var_dump($b);
+    // echo '<br>';
+    // var_dump($c);
+    // echo'<br>';
+    // var_dump($d);
+    // echo '<br>';
+    // var_dump($e);
+    // echo'<br>';
+    // var_dump($f);
+    // echo '<br>';
+
+
+    //echo pi();
+    
+    //echo min(5,1,3,2);
+    //echo max(5,1,3,2);
+
+    // echo abs(-10);
+
+    //echo sqrt(16);
+
+    //echo round(3.45, 0);
+
+    // echo rand(5, 10);
+    
+    
+
 
     ?>
 </body>
